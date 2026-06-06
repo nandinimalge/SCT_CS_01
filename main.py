b155d4e12a408f0f1165baa357feacb24701bb0f
Nandini Malge <nandini.malge2006@gmail.com>
2026-06-06 18:36:33 +0530

    Added encryption function

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("Caesar Cipher Program")
+
+def encrypt():
+    text_message = input("Enter your message: ")
+    key = int(input("Enter shift value: "))
+    encrypted_text=" "
+
+    for ch in text_message:
+
+        if ch.isupper():
+            alphabet_position = ord(ch) - ord('A')
+            shifted_position = (alphabet_position + key) % 26
+            encrypted_char = chr(shifted_position + ord('A'))
+            encrypted_text += encrypted_char
+
+        elif ch.islower():
+            alphabet_position = ord(ch) - ord('a')
+            shifted_position = (alphabet_position + key) % 26
+            encrypted_char = chr(shifted_position + ord('a'))
+            encrypted_text += encrypted_char
+
+        else:
+            encrypted_text += encrypted_char
+
+    print("\nOriginal Message : ",text_message)
+    print("Encrypted Message : ",encrypted_text)
+
+encrypt()
+
+
+
